c536d2e86b1e379ef920165f0a871d5d2f463e3c
soonkuk <soonkuk@gmail.com>
2024-08-07 21:08:34 +0900

    feat: add mongodb index

---
+++ cmds/process_digest_database.go
@@ -0,0 +1,105 @@
+package cmds
+
+import (
+	"context"
+	"github.com/ProtoconNet/mitum-currency/v3/digest"
+	mongodbstorage "github.com/ProtoconNet/mitum-currency/v3/digest/mongodb"
+	minicdigest "github.com/ProtoconNet/mitum-minic/digest"
+	"github.com/ProtoconNet/mitum2/isaac"
+	isaacdatabase "github.com/ProtoconNet/mitum2/isaac/database"
+	"github.com/ProtoconNet/mitum2/launch"
+	"github.com/ProtoconNet/mitum2/util"
+	"github.com/ProtoconNet/mitum2/util/encoder"
+	"github.com/ProtoconNet/mitum2/util/logging"
+	"github.com/pkg/errors"
+)
+
+func ProcessDatabase(ctx context.Context) (context.Context, error) {
+	var l digest.YamlDigestDesign
+	if err := util.LoadFromContext(ctx, digest.ContextValueDigestDesign, &l); err != nil {
+		return ctx, err
+	}
+
+	if l.Equal(digest.YamlDigestDesign{}) {
+		return ctx, nil
+	}
+	conf := l.Database()
+
+	switch {
+	case conf.URI().Scheme == "mongodb", conf.URI().Scheme == "mongodb+srv":
+		return processMongodbDatabase(ctx, l)
+	default:
+		return ctx, errors.Errorf("Unsupported database type, %v", conf.URI().Scheme)
+	}
+}
+
+func processMongodbDatabase(ctx context.Context, l digest.YamlDigestDesign) (context.Context, error) {
+	conf := l.Database()
+
+	/*
+		ca, err := cache.NewCacheFromURI(conf.Cache().String())
+		if err != nil {
+			return ctx, err
+		}
+	*/
+
+	var encs *encoder.Encoders
+	if err := util.LoadFromContext(ctx, launch.EncodersContextKey, &encs); err != nil {
+		return ctx, err
+	}
+
+	st, err := mongodbstorage.NewDatabaseFromURI(conf.URI().String(), encs)
+	if err != nil {
+		return ctx, err
+	}
+
+	if err := st.Initialize(); err != nil {
+		return ctx, err
+	}
+
+	var db isaac.Database
+	if err := util.LoadFromContextOK(ctx, launch.CenterDatabaseContextKey, &db); err != nil {
+		return ctx, err
+	}
+
+	mst, ok := db.(*isaacdatabase.Center)
+	if !ok {
+		return ctx, errors.Errorf("expected isaacdatabase.Center, not %T", db)
+	}
+
+	dst, err := loadDigestDatabase(mst, st, false)
+	if err != nil {
+		return ctx, err
+	}
+	var log *logging.Logging
+	if err := util.LoadFromContextOK(ctx, launch.LoggingContextKey, &log); err != nil {
+		return ctx, err
+	}
+
+	_ = dst.SetLogging(log)
+
+	return context.WithValue(ctx, digest.ContextValueDigestDatabase, dst), nil
+}
+
+func loadDigestDatabase(mst *isaacdatabase.Center, st *mongodbstorage.Database, readonly bool) (*digest.Database, error) {
+	var dst *digest.Database
+	if readonly {
+		s, err := digest.NewReadonlyDatabase(mst, st)
+		if err != nil {
+			return nil, err
+		}
+		dst = s
+	} else {
+		s, err := digest.NewDatabase(mst, st)
+		if err != nil {
+			return nil, err
+		}
+		dst = s
+	}
+
+	if err := dst.Initialize(minicdigest.DefaultIndexes); err != nil {
+		return nil, err
+	}
+
+	return dst, nil
+}
--- digest/index.go
+++ digest/index.go
@@ -10,7 +10,9 @@ var indexPrefix = "mitum_digest_"
 
 var accountIndexModels = []mongo.IndexModel{
 	{
-		Keys: bson.D{bson.E{Key: "address", Value: 1}, bson.E{Key: "height", Value: -1}},
+		Keys: bson.D{
+			bson.E{Key: "address", Value: 1},
+			bson.E{Key: "height", Value: -1}},
 		Options: options.Index().
 			SetName("mitum_digest_account"),
 	},
@@ -66,8 +68,217 @@ var operationIndexModels = []mongo.IndexModel{
 	},
 }
 
-var defaultIndexes = map[string] /* collection */ []mongo.IndexModel{
-	defaultColNameAccount:   accountIndexModels,
-	defaultColNameBalance:   balanceIndexModels,
-	defaultColNameOperation: operationIndexModels,
+var timestampIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "height", Value: -1},
+			bson.E{Key: "isItem", Value: 1},
+			bson.E{Key: "project_id", Value: 1},
+			bson.E{Key: "timestamp_idx", Value: 1}},
+		Options: options.Index().
+			SetName("mitum_digest_timestamp_idx_contract_height_isItem_projectID"),
+	},
+}
+
+var nftCollectionIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_nft_collection_contract_height"),
+	},
+}
+
+var nftIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "nft_idx", Value: 1},
+			bson.E{Key: "height", Value: -1},
+			bson.E{Key: "istoken", Value: 1},
+		},
+		Options: options.Index().
+			SetName("mitum_digest_nft_idx_contract_height_istoken"),
+	},
+	{
+		Keys: bson.D{bson.E{Key: "facthash", Value: 1}},
+		Options: options.Index().
+			SetName("mitum_digest_nft_facthash"),
+	},
+}
+
+var nftOperatorIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "address", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_nft_operator_contract_address_height"),
+	},
+}
+
+var didCredentialServiceIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_credential_service_contract_height"),
+	},
+}
+
+var didCredentialIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "template", Value: 1},
+			bson.E{Key: "credential_id", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_credential_id_contract_template_height"),
+	},
+}
+
+var didCredentialHolderIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "holder", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_credential_holder_contract_height"),
+	},
+}
+
+var didCredentialTemplateIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "template", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_credential_template_contract_height"),
+	},
+}
+
+var daoServiceIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_dao_service_contract_height"),
+	},
+}
+
+var daoProposalIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "proposal_id", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_dao_proposal_contract_height"),
+	},
+}
+
+var daoDelegatorsIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "proposal_id", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_dao_approved_contract_proposalID_height"),
+	},
+}
+
+var daoVotersIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "proposal_id", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_dao_voter_contract_proposalID_height"),
+	},
+}
+
+var daoVotingPowerBoxIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "proposal_id", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_dao_voting_power_contract_proposalID_height"),
+	},
+}
+
+var tokenServiceIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_token_service_contract_height"),
+	},
+}
+
+var tokenBalanceIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "address", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_token_balance_contract_address_height"),
+	},
+}
+
+var pointServiceIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_point_service_contract_height"),
+	},
+}
+
+var pointBalanceIndexModels = []mongo.IndexModel{
+	{
+		Keys: bson.D{
+			bson.E{Key: "contract", Value: 1},
+			bson.E{Key: "address", Value: 1},
+			bson.E{Key: "height", Value: -1}},
+		Options: options.Index().
+			SetName("mitum_digest_point_balance_contract_address_height"),
+	},
+}
+
+var DefaultIndexes = map[string] /* collection */ []mongo.IndexModel{
+	defaultColNameAccount:              accountIndexModels,
+	defaultColNameBalance:              balanceIndexModels,
+	defaultColNameOperation:            operationIndexModels,
+	defaultColNameTimeStamp:            timestampIndexModels,
+	defaultColNameNFTCollection:        nftCollectionIndexModels,
+	defaultColNameNFT:                  nftIndexModels,
+	defaultColNameNFTOperator:          nftOperatorIndexModels,
+	defaultColNameDIDCredentialService: didCredentialServiceIndexModels,
+	defaultColNameDIDCredential:        didCredentialIndexModels,
+	defaultColNameHolder:               didCredentialHolderIndexModels,
+	defaultColNameTemplate:             didCredentialTemplateIndexModels,
+	defaultColNameDAO:                  daoServiceIndexModels,
+	defaultColNameDAOProposal:          daoProposalIndexModels,
+	defaultColNameDAODelegators:        daoDelegatorsIndexModels,
+	defaultColNameDAOVoters:            daoVotersIndexModels,
+	defaultColNameDAOVotingPowerBox:    daoVotingPowerBoxIndexModels,
+	defaultColNameToken:                tokenServiceIndexModels,
+	defaultColNameTokenBalance:         tokenBalanceIndexModels,
+	defaultColNamePoint:                pointServiceIndexModels,
+	defaultColNamePointBalance:         pointBalanceIndexModels,
 }
